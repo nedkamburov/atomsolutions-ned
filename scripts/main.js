86bae7354c50b81c6ffaccb1c64d4f639c109f55
Nedislav Kamburov <michelangelo@Nedislavs-MacBook-Pro.local>
2021-02-02 16:36:02 +0200

    fix responsive mobile

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', ready);
 
 function ready() {
   activateSlider();
+  menuBtn();
 }
 
 function activateSlider() {
@@ -35,4 +36,28 @@ function activateSlider() {
       members[index].classList.add('active');
     })
   });
+}
+
+function menuBtn() {
+  let isOpened = false;
+  const menuToggleBtn = document.getElementsByClassName('toggle-menu')[0];
+  let menu = document.getElementsByClassName('menu-items')[0];
+
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 768) {
+      menu.style.height = `auto`;
+    } else {
+      menu.style.height = `0px`;
+    }
+  });
+
+  menuToggleBtn.addEventListener('click', () => {
+    if (isOpened) {
+      menu.style.height = `0px`;
+    } else {
+      let newHeight = menu.scrollHeight;
+      menu.style.height = `${newHeight}px`;
+    }
+    isOpened = !isOpened;
+  })
 }
